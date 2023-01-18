151ce88cf961de09a93b7d2f1f79944ee832ed11
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-01-19 00:58:53 +0300

    .

--- Kriptoloji/website/app.py
+++ Kriptoloji/website/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+proj_name = "ROSETTA"
 
+@app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', project_name=proj_name)
 
+@app.route('/decrypt')
+def decrypt():
+    return render_template('decrypt.hmtl', project_name=proj_name)
+
+@app.route('/encrypt')
+def encrypt():
+    return render_template('decrypt.hmtl', project_name=proj_name)
+
+@app.route('/uploadtxt', methods=["POST"])
+def uploadtxt():
+
+
+        return redirect('/')
+
+        return redirect('/error')
+@app.route('/upload', methods=["POST"])
+def upload():
+
+        return redirect("/")
+
+        return redirect('/error')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4567, debug=1)
